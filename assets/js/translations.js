6ffd0d09a4e600bdd4131ac56742f66d11342e7d
Waleed Salah <87587845+DevWaleedS@users.noreply.github.com>
2022-07-18 00:09:06 +0200

    commit

---
+++ assets/js/translations.js
@@ -0,0 +1,106 @@
+const translations = {
+  // English translations
+  "en": {
+      "page-title" : "Kareem Protein",
+      "home": "Home",
+      "features": "features",
+      "about": "about",
+      "contact-us": "Contact us",
+      "English": "English",
+      "Arabic": "Arabic",
+      
+      //hero Section
+    "hero-title": "Your Healthy Partner",
+    "hero-desc": "These combinations are based on consumer favorites, tried and tested by the real experts- You!",
+    "contact-btn": "Contact us now",
+
+    //Features Section
+    "offreing": "Offreing",
+    "features-title": "Our Amazing Features",
+    "healthy": "Healthy",
+    "healthy-desc": "Healthy drink that also comes with vitamins and minerals, this drink does even better than you may know.",
+    "naturals": "Naturals",
+    "naturals-desc": "It’s also rich in immune-boosting vitamin C, making it one of our favorite healthy drinks to fight off colds.",
+    "tasty": "Tasty",
+    "tasty-desc": "Recommend starting your day with Kareem Protein. Kareem Protein is a surprisingly powerful immune booster.",
+    
+    //About-us Section
+    "about-title": " About us",
+    "about-section-title": "Your Healthy Partner",
+    "about-desc": "Milk Protein concentrate, water, skimmed, milk, stabiliser, carrageenan.",
+    "acidity": "acidity regulator :",
+    "acidity-desc": "trisodium citrate.",
+    "flavors": "flavors :",
+    "flavors-desc": "chocolate , strawberry , vanilla.",
+    "sweetener": "Sweetener :",
+    "sweetener-desc": "sucralose.",
+    "colour": "colour :",
+    "colour-desc": "beta carotene.",
+    "calories": "calories :",
+    "calories-desc": "152",
+    "protein": "Protein :",
+    "protein-desc": "26G",
+    "fat": "fat :",
+    "fat-desc": "0.6G",
+
+    //contact Section
+    "contact-title": "Let's talk",
+    "contact-desc": "If you want to reserve our Product, contact us and we will attend to you quickly, with our 24/7 Email service.",
+  
+    "coyright" : `Copyright &copy; <span>Kareem Protein</span> All Rights Reserved`
+  },
+
+  // Arabic translations
+  "ar": {
+    "page-title" : "كريم بروتين",
+    "home": " الرئيسية",
+    "features": "المميزات",
+    "about": "من نحن",
+    "contact-us": "تواصل معنا",
+    "English": "الإنجليزية",
+    "Arabic": "العربية",
+
+    //hero Section
+    "hero-title": "شريكك الصحي",
+    "hero-desc": "يستند هذا المنتج إلى تفضيلات المستهلك ، وقد تم تجربته واختباره بواسطة خبراء حقيقيين - لك أنت!",
+    "contact-btn": "اتصل بنا الآن",
+
+    //Features Section
+    "offreing": "عرض رائع",
+    "features-title": "مميزاتنا الرائعه",
+    "healthy": "صحي",
+    "healthy-desc": "مشروب صحي يحتوي أيضًا على فيتامينات ومعادن ، هذا المشروب أفضل مما قد تعرفه.",
+    "naturals": "طبيعي",
+    "naturals-desc": "كما أنه غني بفيتامين سي المعزز للمناعة ، مما يجعله أحد المشروبات الصحية المفضلة لدينا لمحاربة نزلات البرد.",
+    "tasty": "لذيذ",
+    "tasty-desc": "نوصي ببدء يومك مع كريم بروتين. كريم بروتين هو معزز قوي للمناعة بشكل مدهش.",
+    
+    //About-us Section
+    "about-title": " من نحن",
+    "about-section-title": "شريكك الصحي",
+    "about-desc": "مركز بروتين الحليب ، ماء ، منزوع الدسم ، حليب ، مثبت ، كاراجينان.",
+    "acidity": "منظم الحموضة :",
+    "acidity-desc": "السايتريت ثلاثي الصوديوم.",
+    "flavors": "النكهات :",
+    "flavors-desc": "شوكولاتة ، فراولة ، فانيليا.",
+    "sweetener": "السكر:",
+    "sweetener-desc": "السكرالوز.",
+    "colour": "الالوان :",
+    "colour-desc": "بيتا كاروتين.",
+    "calories": "السعرات الحراريه :",
+    "calories-desc": "152",
+    "protein": "بروتين :",
+    "protein-desc": "26G",
+    "fat": "دهون :",
+    "fat-desc": "0.6G",
+
+    //contact Section
+    "contact-title": "لنتحدث",
+    "contact-desc": "إذا كنت ترغب في حجز منتجنا ، فاتصل بنا وسنقوم بالرد عليك بسرعة ، من خلال خدمة البريد الإلكتروني على مدار الساعة طوال أيام الأسبوع.",
+    
+    "coyright" : ` جميع الحقوق محفوظه&copy; <span>كريم بروتين</span>`
+
+  },
+};
+
+export default translations;
